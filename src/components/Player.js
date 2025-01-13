a5f4a42a6dd2690da86055ca9b990a1c622b4c58
Gaurav Jangid <60379544+gaurav700@users.noreply.github.com>
2025-01-13 20:56:33 +0000

    Reading the value from the input and showing it to a place with help of states.

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,10 +1,25 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [name, setName] = useState();
+  const [buttonClicked, setButtonClicked] = useState(false);
+  function handleChange(name) {
+    setButtonClicked(false);
+    setName(name);
+  }
+  function handleButton() {
+    setButtonClicked(true);
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {buttonClicked ? name : "unknown entity"} </h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input
+          type="text"
+          onChange={(e) => handleChange(e.target.value)}
+          value={name}
+        />
+        <button onClick={handleButton}>Set Name</button>
       </p>
     </section>
   );
